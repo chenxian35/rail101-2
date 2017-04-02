dceb9427698a293b1475a696ae2d54a1c060ff7d
chenxian35 <linwzh32@gmail.com>
2017-04-02 16:23:29 +0800

    add bootstrap t0 project

--- rails102/app/controllers/welcome_controller.rb
+++ rails102/app/controllers/welcome_controller.rb
@@ -1,2 +1,7 @@
 class WelcomeController < ApplicationController
+   def index
+    flash[:notice]="Good morning,git"
+    flash[:alert]="Good night ,it's time to sleep"
+    flash[:warning]="This is warning"
+   end
 end
