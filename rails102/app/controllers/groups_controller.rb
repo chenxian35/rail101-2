c7477e411d3bc813c295c2b9827616957bbf4ad6
chenxian35 <linwzh32@gmail.com>
2017-04-03 17:17:46 +0800

    join & quit group action

--- rails102/app/controllers/groups_controller.rb
+++ rails102/app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
 
-before_action :authenticate_user!, only: [:new, :create, :edit , :update ,:destroy]
+before_action :authenticate_user!, only: [:new, :create, :edit , :update ,:destroy, :join, :quit]
 before_action :find_group_and_check_premission, only: [:edit, :update, :destroy]
 
   def index
@@ -24,7 +24,7 @@ end
 def show
   @group = Group.find(params[:id])
   @posts = @group.posts.recent.paginate(:page => params[:page],:per_page => 5)
-  
+
 end
 
 def edit
@@ -49,6 +49,29 @@ def destroy
   redirect_to groups_path
 
 end
+
+def join
+@group =Group.find(params[:id])
+if !current_user.is_member_of?(@group)
+  current_user.join!(@group)
+  flash[:notice]= "加入本论坛成功"
+else
+  flash[:warning]= "你已经是本论坛成员"
+end
+redirect_to group_path(@group)
+end
+
+def quit
+  @group = Group.find(params[:id])
+  if current_user.is_member_of?(@group)
+    current_user.quit!(@group)
+    flash[:alert]="已退出本讨论版"
+  else
+    flash[:warning]="你不是本讨论版成员，不用退出"
+  end
+  redirect_to group_path(@group)
+end
+
 
 
   private
