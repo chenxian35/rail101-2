1e7d83d0a4f5a2027481fced0c13f12ab8ab9157
chenxian35 <linwzh32@gmail.com>
2017-04-03 09:53:49 +0800

    implement groups#edit

--- rails102/app/controllers/groups_controller.rb
+++ rails102/app/controllers/groups_controller.rb
@@ -17,6 +17,10 @@ def show
   @group=Group.find(params[:id])
 end
 
+def edit
+ @group=Group.find(params[:id])
+end
+
   private
    def group_params
      params.require(:group).permit(:title ,:description)
