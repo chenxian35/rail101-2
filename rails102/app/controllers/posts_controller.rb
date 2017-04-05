a01af2c0cd9779bd0bc3a3dee7763ce4c827f669
chenxian35 <linwzh32@gmail.com>
2017-04-05 18:24:59 +0800

    post link to edit

--- rails102/app/controllers/posts_controller.rb
+++ rails102/app/controllers/posts_controller.rb
@@ -1,7 +1,12 @@
 class PostsController < ApplicationController
 
   before_action :authenticate_user!, :only => [:new, :create]
+  before_action :find_post_and_check_premission, only: [:edit, :update, :destroy]
 
+  def index
+      @group = Group.find(params[:group_id])
+      @posts=@group.posts.all
+    end
   def new
     @group = Group.find(params[:group_id])
     @post = Post.new
@@ -21,7 +26,41 @@ class PostsController < ApplicationController
   end
 
 
+def edit
+  @group = Group.find(params[:group_id])
+end
+
+def update
+
+   if @post.update(post_params)
+
+  redirect_to group_path(@group),notice: "Update success"
+else
+  render :edit
+end
+end
+
+def destroy
+@group = Group.find(params[:group_id])
+@post=Post.find(params[:id])
+@post.group=@group
+  @post.destroy
+
+ flash[:alert] ="post deleted"
+  redirect_to account_posts_path
+
+end
+
   private
+  def find_post_and_check_premission
+  @group=Group.find(params[:group_id])
+  #@post.group=@group
+  @post=Post.find(params[:id])
+  if current_user !=@post.user
+    redirect_to root_path ,alert: "you have not permission"
+  end
+  end
+
 
   def post_params
     params.require(:post).permit(:content)
