a5af024e5221fdd33f1bc7e01d7e20b483d574a8
chenxian35 <linwzh32@gmail.com>
2017-04-13 21:27:47 +0800

    edit,new controll

--- rails104/app/controllers/groups_controller.rb
+++ rails104/app/controllers/groups_controller.rb
@@ -16,6 +16,30 @@ def create
   end
 end
 
+def show
+  @group=Group.find(params[:id])
+end
+
+def edit
+ @group=Group.find(params[:id])
+end
+def update
+  @group=Group.find(params[:id])
+  if @group.update(group_params)
+  flash[:notice] = "Group update success"
+  redirect_to groups_path,notice: "update success"
+else
+  render :edit
+end
+
+end
+
+def destroy
+    @group=Group.find(params[:id])
+    @group.destroy
+    flash[:alert] = "Group deleted"
+    redirect_to groups_path,Noitc:"delete success"
+end
 private
 def group_params
   params.require(:group).permit(:title, :description)
